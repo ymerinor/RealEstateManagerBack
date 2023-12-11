472b2ba861e88c7209a7f6a1d2d3a6250a1976af
ymerinor <150065426+ymerinor@users.noreply.github.com>
2023-12-11 06:50:28 -0500

    Comentario a PropertyImageRepository

--- RealEstateManager.Infrastructure/Repository/PropertyImageRepository.cs
+++ RealEstateManager.Infrastructure/Repository/PropertyImageRepository.cs
@@ -3,10 +3,19 @@ using RealEstateManager.Domain.Repository;
 
 namespace RealEstateManager.Infrastructure.Repository
 {
+    /// <summary>
+    /// Repositorio para operaciones relacionadas con imágenes de propiedades en la base de datos.
+    /// Implementa la interfaz <see cref="IPropertyImageRepository"/>.
+    /// </summary>
     public class PropertyImageRepository(RealEstateManagerDbContext realEstateManagerDbContext) : IPropertyImageRepository
     {
         private readonly RealEstateManagerDbContext _realEstateManagerDbContext = realEstateManagerDbContext;
 
+        /// <summary>
+        /// Crea una nueva entidad de imagen de propiedad en la base de datos.
+        /// </summary>
+        /// <param name="propertyImage">Entidad de imagen de propiedad a crear.</param>
+        /// <returns>La entidad de imagen de propiedad creada.</returns>
         public async Task<PropertyImage> CreateAsync(PropertyImage propertyImage)
         {
             await _realEstateManagerDbContext.PropertyImage.AddAsync(propertyImage);
